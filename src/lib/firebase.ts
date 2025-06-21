dd9d1d32a7382b9e9714c58e52b20a40f33147af
Mahmoud Elkhouly <Mahmoud750ty@gmail.com>
2025-06-21 18:11:59 +0300

    Update firebase.ts

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -12,17 +12,17 @@ import { getStorage } from "firebase/storage";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-    apiKey: "AIzaSyAUVtQUa4dMPA7MVF5wAJSxB-UvCuZQo7o",
-    authDomain: "pipbeachplug.firebaseapp.com",
-    projectId: "pipbeachplug",
+    apiKey: "",
+    authDomain: "",
+    projectId: "",
     //
     //  vvvvvvvvvvvvv   THIS IS THE CORRECTED LINE   vvvvvvvvvvvvv
     //
-    storageBucket: "pipbeachplug.firebasestorage.app",
+    storageBucket: "",
     //
-    messagingSenderId: "962943218026",
-    appId: "1:962943218026:web:a21c164e452b0c67347513",
-    measurementId: "G-P30RJPFEQ2"
+    messagingSenderId: "",
+    appId: "3",
+    measurementId: ""
 };
 
 // Initialize Firebase
